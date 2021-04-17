6d569b8b43d1f3e869d05cc4272052cf72f01f5a
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-04-17 11:47:26 -0700

    Better error page

--- src/routes/$error.svelte
+++ src/routes/$error.svelte
@@ -1,8 +1,43 @@
+<script context="module" lang="ts">
+import type { ErrorLoad, ErrorLoadInput } from "@sveltejs/kit/types/page"
+
+// eslint-disable-next-line no-shadow
+export function load(input: ErrorLoadInput): ReturnType<ErrorLoad>
+{
+	switch (input.status)
+	{
+		case 404: return { props: { title: "Not found", message: "Sorry, that page wasn’t found." } }
+		default: return { props: { title: "Oops", message: "Sorry, something went wrong.", error: input.error } }
+	}
+}
+</script>
+
+<script lang="ts">
+	export let title: string
+	export let message: string
+	export let error: Error | undefined
+</script>
+
 <svelte:head>
-	<title>Not found - SvelteKitTemplate</title>
+	<title>{title} - SvelteKitTemplate</title>
 </svelte:head>
 
-<h1>Not found</h1>
+<h1>{title}</h1>
 <p>
-	Sorry, that page wasn't found.
+	{message}
 </p>
+{#if error}
+<details style="margin-top: 4em;">
+	<summary>
+		Technical details
+	</summary>
+	<h3>
+		{error.name}: {error.message}
+	</h3>
+	{#if "stack" in error}
+	<pre>
+		{error.stack}
+	</pre>
+	{/if}
+</details>
+{/if}
