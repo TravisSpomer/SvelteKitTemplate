0afeb7598397fd088135bc2d91d1f59c973757fc
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-04-21 14:58:32 -0700

    Phone and keyboard nav

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,21 +1,36 @@
 <script lang="ts">
 	import "../app.scss"
 	import Year from "$lib/components/Year.svelte"
+
+	let isExpanded = false
+
+	function toggleHeader()
+	{
+		isExpanded = !isExpanded
+		if (isExpanded && document.activeElement && "blur" in document.activeElement)
+			(document.activeElement as any).blur()
+	}
 </script>
 
 <svelte:head>
 
 </svelte:head>
 
-<header>
+<header aria-expanded={isExpanded} tabindex="-1">
 <div><nav>
+	<a href="#top" class="skip-to-content">Skip to content</a>
 	<ul>
 		<li><a href="/">Home</a></li>
 	</ul>
+	<div tabindex="-1" class="expander" onclick={toggleHeader}>
+		<svg width="48" height="48" fill="black">
+			<path d="M14,17h20m0,7h-20m0,7h20" />
+		</svg>
+	</div>
 </nav></div>
 </header>
 
-<main>
+<main id="top">
 <div class="content">
 <slot></slot>
 </div>
