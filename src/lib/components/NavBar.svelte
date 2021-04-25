e55c5bac47fd4c39d937aa0d5a1a408a38407fdc
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-04-25 15:34:46 -0700

    Package nav bar into NavBar component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let header: HTMLElement
+	let expanded: boolean
+
+	function expandHeader()
+	{
+		expanded = !expanded
+		const element = document.activeElement as (HTMLElement | null)
+		if (!expanded && element && "blur" in element as any) element.blur()
+	}
+</script>
+
+<header bind:this={header} aria-expanded={expanded} tabindex="-1">
+	<div><nav>
+		<a href="#top" class="skip-to-content">Skip to content</a>
+		<ul>
+			<li><a href="/">Home</a></li>
+		</ul>
+		<div id="expander" tabindex="-1" class="expander" on:click={expandHeader}>
+			<svg width="48" height="48">
+				<path d="M14,17h20m0,7h-20m0,7h20" />
+			</svg>
+		</div>
+	</nav></div>
+</header>
--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,34 +1,10 @@
 <script lang="ts">
 	import "../app.scss"
+	import NavBar from "$lib/components/NavBar.svelte"
 	import Year from "$lib/components/Year.svelte"
-
-	let isExpanded = false
-
-	function toggleHeader()
-	{
-		isExpanded = !isExpanded
-		if (isExpanded && document.activeElement && "blur" in document.activeElement)
-			(document.activeElement as any).blur()
-	}
 </script>
 
-<svelte:head>
-
-</svelte:head>
-
-<header aria-expanded={isExpanded} tabindex="-1">
-<div><nav>
-	<a href="#top" class="skip-to-content">Skip to content</a>
-	<ul>
-		<li><a href="/">Home</a></li>
-	</ul>
-	<div tabindex="-1" class="expander" onclick={toggleHeader}>
-		<svg width="48" height="48" fill="black">
-			<path d="M14,17h20m0,7h-20m0,7h20" />
-		</svg>
-	</div>
-</nav></div>
-</header>
+<NavBar />
 
 <main id="top">
 <div class="content">
