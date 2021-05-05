3c524e6df07b084eb91eab9f3f2cb5a6ff7ecd69
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-05-05 15:40:47 -0700

    Update to latest SvelteKit

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import mdsvex from "mdsvex"
+import sveltePreprocess from "svelte-preprocess"
+import adapterStatic from "@sveltejs/adapter-static"
+import mdsvexConfig from "./mdsvex.config.cjs"
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+
+	extensions: [
+		".svelte", ...mdsvexConfig.extensions,
+	],
+
+	preprocess: [
+		mdsvex.mdsvex(mdsvexConfig),
+		sveltePreprocess(),
+	],
+
+	kit: {
+		adapter: adapterStatic(),
+		paths: {
+			base: process.env.BASE_URL || "",
+		},
+		vite: {
+			ssr: {
+				noExternal: [
+				],
+			}
+		},
+	},
+
+}
