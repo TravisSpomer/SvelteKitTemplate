90170c47336d8aaf6994858fd6407afdb8e926d1
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-05-07 21:35:24 -0700

    Home page text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 <h1>Travis's SvelteKit template</h1>
 
 <p>
-	First, this template used Gulp. Now it's all SvelteKit, baby!
+	A very opinionated site template for SvelteKit to make it a little more production-ready.
 </p>
 <p>
 	<a href="/page-two">Here's a page using Markdown.</a>
