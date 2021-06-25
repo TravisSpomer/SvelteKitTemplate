3de79a76c92fe27cbf69f559e5ca17a23dcfcd77
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-06-25 15:38:51 -0700

    Upgrade to latest SvelteKit

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,6 @@
 import { dev } from "$app/env"
 import { minify, Options } from "html-minifier"
-import type { ServerRequest, ServerResponse } from "@sveltejs/kit/types/endpoint"
+import type { Response, Handle } from "@sveltejs/kit"
 
 const minifierOptions: Options =
 {
@@ -23,17 +23,17 @@ const minifierOptions: Options =
 	sortClassName: true
 }
 
-export async function handle({ request, render }: { request: ServerRequest, render: ((request: ServerRequest) => Promise<ServerResponse>)}): Promise<ServerResponse>
+export async function handle({ request, resolve }: Parameters<Handle>[0]): Promise<Response>
 {
-	const response = await render(request)
+	const response = await resolve(request)
 
 	// Minify all HTML outputs in production builds.
 	// IMPORTANT: If you use this template for a non-static site, you should probably only do this when prerendering.
 	//     import { prerendering } from "$app/env"
 	//     ...and then add "prerendering &&" to the condition below.
 	// Note that prerendering is false when using "npm run serve" ("svelte-kit preview").
-	if (!dev && response.headers && response.headers["content-type"] === "text/html")
-		response.body = minify(response.body, minifierOptions)
+	if (!dev && response.headers && response.body && response.headers["content-type"] === "text/html")
+		response.body = minify(response.body.toString(), minifierOptions)
 
 	return response
 }
